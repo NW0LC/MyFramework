7afba498cb356fee934ed7c8fb5fedda822d9af7
史忠文 <704796146@qq.com>
2017-07-19 16:53:59 +0800

    提示

--- app/src/main/java/rx/Observable.java
+++ app/src/main/java/rx/Observable.java
@@ -1,7 +1,7 @@
 package rx;
 
 /**
- * 为了realm 实体类继承  防止json解析报错所用
+ * 为了realm 实体类继承  防止json解析报错所用 如若使用 realm  需要本地使用 其plugin
  */
 public class Observable {
     // Dummy class required for Jackson-Databind support if
